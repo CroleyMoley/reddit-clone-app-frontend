e7df168720db6c1ffcc7ecf47ac62734b33d3b77
MisterAnthropy <tjcroley@outlook.com>
2021-02-19 10:48:02 -0800

    created login form that shows up on page

---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <form onsubmit={}>
+            <input type="text" name="username" value={} onChange={} ></input>
+            <input type="text" name="password" value={} onChange={} ></input>
+            <input type="submit" value="Login"></input>
+        </form>
+    )
+}
